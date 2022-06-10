db6a6cbfb59363cea2c59f41f7ffcfbb7381e7b7
guiparpineli <guiparpineli@hotmail.com>
2022-06-09 21:05:22 -0300

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+const navHidden = document.querySelector('.ul-visible');
+const nav = document.querySelector('.ul-hidden');
+
+navHidden.onclick = a => {
+    navHidden.style.display = 'none';
+    nav.style.display = 'block';
+}
+nav.onclick = a => {
+    nav.style.display = 'none';
+    navHidden.style.display = 'block';
+}
