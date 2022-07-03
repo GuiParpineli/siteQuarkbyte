ce19b8dc3bf1ca8e7bfcfa359003eb633812fe97
Parpineli <parpineli@pop-os.localdomain>
2022-07-03 18:58:20 -0300

    ajust link header and pics about

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,13 @@
 const navHidden = document.querySelector('.ul-visible');
 const nav = document.querySelector('.ul-hidden');
 
+
 navHidden.onclick = a => {
     navHidden.style.display = 'none';
     nav.style.display = 'block';
 }
 nav.onclick = a => {
+
     nav.style.display = 'none';
     navHidden.style.display = 'block';
 }
